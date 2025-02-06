1a854072f009dbba1e3622cfd6cd1446c15f4091
luminoray <zoestell37@gmail.com>
2025-02-06 07:12:43 -0700

    Initial commit

---
+++ src.js
@@ -0,0 +1,19 @@
+var togglers = document.getElementsByClassName('toggler');
+var togglables = document.getElementsByClassName('togglable');
+
+for(let toggler of togglers) {
+    toggler.addEventListener('click', function(e) {
+        e.preventDefault();
+        for(let togglable of togglables) {
+            if ('#'+togglable.id === e.target.getAttribute('href')) {
+                togglable.style.display = 'block';
+            } else {
+                togglable.style.display = 'none';
+            }   
+        }
+    });
+}
+
+for(let togglable of togglables) {
+    togglable.style.display = 'none';
+}
